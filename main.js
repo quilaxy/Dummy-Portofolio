6609ebf7d7e49a1ceda34424e56fc6cd73452252
quilaxy <annafi.nur.523@gmail.com>
2023-09-02 14:29:38 +0700

    add contact

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ function toggleDetail(event) {
 
     $(detail).slideToggle()
 }
+
+function onFormSubmit(event) {
+    event.preventDefault() //biar setelah submit gak ke scroll ke atas
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if (!$(email).val()) {
+        alert("Email is required")
+     }else if (!$(subject).val()) {
+        alert("Subject is required")
+    } else if (!$(message).val()) {
+        alert("Message is required")
+    } else {
+        alert("Form Submitted")
+        //untuk mengosongkan form setelah di submit
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
